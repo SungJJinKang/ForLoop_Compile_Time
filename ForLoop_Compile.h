4f9a4cc9f79bec7278a4efa6f5774dd0703ada1e
sungJinKangdev <hour30000@gmail.com>
2021-01-22 16:09:09 +0900

    add usability, fix bugs

--- ForLoop_Compile.h
+++ ForLoop_Compile.h
@@ -1,13 +1,19 @@
 #pragma once
 #include <functional>
-
+#include <utility>
+#include <type_traits>
 /// <summary>
 /// loop from start(inclusive) to end(inclusive)
 /// </summary>
-template <size_t start, size_t end>
+template <int start, int end>
 struct ForLoop_Compile
 {
 	static_assert(start <= end, "end must be greater than start");
+	
+	/// <summary>
+	/// Just Loop Job
+	/// </summary>
+	/// <param name="job"></param>
 	static void Loop(const std::function<void()>& job)
 	{
 		job();
@@ -17,4 +23,38 @@ struct ForLoop_Compile
 			ForLoop_Compile<start + 1, end>::Loop(job);
 		}
 	}
+
+	/// <summary>
+	/// For Use loop value(start ~ end).
+	/// 
+	/// Loop variable must be at first function argument
+	/// And Never put reference to loop variable
+	/// 
+	/// You Can't change value of loop varaible at inside of loop function
+	/// 
+	/// 
+	/// Example)
+	/// 
+	/// void addLoop(int loop, int initialValue)
+	/// {
+	/// 	initialValue += loop;
+	/// }
+	/// </summary>
+	/// <typeparam name="F"></typeparam>
+	/// <typeparam name="...Args"></typeparam>
+	/// <param name="f"></param>
+	/// <param name="...args"></param>
+	template <typename F, typename... Args, std::enable_if_t<std::is_function_v< std::remove_reference_t<F> >, bool> = true>
+	static void Loop(F&& f, Args&&... args)
+	{
+		std::invoke(std::forward<F>(f), start, std::forward<Args>(args)...);
+
+		if constexpr (start < end)
+		{
+			ForLoop_Compile<start + 1, end>::Loop(std::forward<F>(f), std::forward<Args>(args)...);
+		}
+	}
+	
+	
+	
 };
--- example/example.cpp
+++ example/example.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
-
 #include "../ForLoop_Compile.h"
+
 
 void add10(int& a)
 {
 	a += 10;
 }
 
+/// <summary>
+/// Loop variable must be at first function argument !!
+/// </summary>
+/// <param name="loop"></param>
+/// <param name="initialValue"></param>
+void addLoop(int loop, int initialValue)
+{
+	std::cout << initialValue + loop << std::endl;
+}
+
 int main()
 {
 	int a = 10;
-	ForLoop_Compile<0, 9>::Loop([&a]() {++a; });
+	ForLoop_Compile<1, 5>::Loop([&a]() {++a; });
 	std::cout << a << std::endl;
 
-	int b = 10;
-	ForLoop_Compile<0, 9>::Loop(std::bind(add10, std::ref(b)));
-	std::cout << b << std::endl;
+	ForLoop_Compile<0, 9>::Loop(addLoop, 10);
+
+
 }
